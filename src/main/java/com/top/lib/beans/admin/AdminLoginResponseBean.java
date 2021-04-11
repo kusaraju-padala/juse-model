7f71cd5e9859b81baa06c2d8bc71d57ccd637f99
GMRHK01 <raazapadala@gmail.com>
2021-04-11 23:41:24 +0530

    Added admin login

---
+++ src/main/java/com/top/lib/beans/admin/AdminLoginResponseBean.java
@@ -0,0 +1,29 @@
+package com.top.lib.beans.admin;
+
+public class AdminLoginResponseBean {
+	private String name;
+	private String key;
+	private String accessLevel;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getKey() {
+		return key;
+	}
+	public void setKey(String key) {
+		this.key = key;
+	}
+	public String getAccessLevel() {
+		return accessLevel;
+	}
+	public void setAccessLevel(String accessLevel) {
+		this.accessLevel = accessLevel;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/top/lib/beans/user/FeedBackBean.java
@@ -0,0 +1,30 @@
+package com.top.lib.beans.user;
+
+public class FeedBackBean {
+	
+	private Integer userId;
+	private Integer score;
+	private String feedback;
+	
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public Integer getScore() {
+		return score;
+	}
+	public void setScore(Integer score) {
+		this.score = score;
+	}
+	public String getFeedback() {
+		return feedback;
+	}
+	public void setFeedback(String feedback) {
+		this.feedback = feedback;
+	}
+	
+	
+
+}
